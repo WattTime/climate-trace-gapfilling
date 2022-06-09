0122067336b21dacc631ecb664f67bdf5c6e8506
AlexStephensJHUAPL <alexandra.stephens@jhuapl.edu>
2022-06-09 14:05:26 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="gap_filling", packages=find_packages())
+
+setup(
+    name='gap_filling',
+    version='0.1.0',
+    description='This project takes Climate Trace data, compares it to other existing datasources, and fills gaps as '
+                'needed.',
+    author='Megan Baker and Alexandra Stephens',
+    author_email='megan.baker@jhuapl.edu, alexandra.stephens@jhuapl.edu',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
