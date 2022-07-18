02101d860ddf3861e21823409e88e91fc5d3a9f6
christyjlewis <43048648+christyjlewis@users.noreply.github.com>
2022-07-18 12:17:33 -0600

    adding function in data handler to update changed values

--- gap_filling/data_handler.py
+++ gap_filling/data_handler.py
@@ -88,6 +88,69 @@ class DataHandler:
         colnames = [desc[0] for desc in curs.description]
         return pd.DataFrame(data=np.array(curs.fetchall()), columns=colnames)
 
+    def compare_values(self, value_in_db, value_inserting):
+        if pd.isna(value_in_db):
+            if pd.isna(value_inserting):
+                return True
+        else:
+            return value_in_db == value_inserting
+
+    def check_if_values_changed(self, data_row_dict):
+        cols = ['subsector', 'iso3_country', 'start_date', 'end_date', 'co2_emissions_tonnes', \
+                'ch4_emissions_tonnes', 'n2o_emissions_tonnes', 'total_co2e_100yrgwp', 'total_co2e_20yrgwp']
+
+        query = "SELECT subsector, iso3_country, start_date, end_date, co2_emissions_tonnes,\
+                ch4_emissions_tonnes, n2o_emissions_tonnes, total_co2e_100yrgwp, total_co2e_20yrgwp " \
+                "FROM stable_ct " \
+                "WHERE subsector = %(ois)s" \
+                "AND iso3_country = %(pei)s "  \
+                "AND start_date = %(st)s " \
+                "AND end_date = %(et)s"
+
+        curs = self.get_cursor()
+        curs.execute(query, data_row_dict)
+        res = curs.fetchall()
+
+        if len(res[0]) < 1:
+            return 'does not exist'
+
+        result_dict = {}
+        i = 0
+        for col in cols:
+            result_dict[col] = res[0][i]
+            i += 1
+
+        gas_in_insert_string = data_row_dict['epf']
+        if gas_in_insert_string.split('_')[0] == 'co2e':
+            cem = data_row_dict['cem']
+            value_in_db = result_dict[f'total_co2e_{cem.split("-")[0]}yrgwp']
+        else:
+            value_in_db = result_dict[f'{gas_in_insert_string}_emissions_tonnes']
+
+        value_inserting = data_row_dict['eq']
+        match = self.compare_values(value_in_db, value_inserting)
+
+        if match:
+            return match
+        if not match:
+            data_row_dict['neq'] = value_inserting
+            data_row_dict['md'] = datetime.datetime.now().isoformat()
+            update_str = "UPDATE ermin " \
+                         "SET emission_quantity = %(eq)s, modified_date = %(md)s" \
+                         "WHERE original_inventory_sector = %(ois)s" \
+                         "AND producing_entity_name = %(pen)s" \
+                         "AND producing_entity_id = %(pei)s" \
+                         "AND reporting_entity = %(re)s" \
+                         "AND emitted_product_formula = %(epf)s" \
+                         "AND carbon_equivalency_method = %(cem)s" \
+                         "AND start_time = %(st)s" \
+                         
+            curs = self.get_cursor()
+            curs.execute(update_str, data_row_dict)
+            self.conn.commit()
+
+            return match
+
     def write_data(self, data_to_insert, rows_type=None):
         # Two different kinds of inserts we'll need to perform here
         data_to_insert['created_date'] = datetime.datetime.now().isoformat()
@@ -111,12 +174,24 @@ class DataHandler:
         for dti in data_to_insert.iterrows():
             print(f'inserting row {dti[0]} out of {data_to_insert.shape[0]} for sector {data_to_insert.loc[dti[0], "original_inventory_sector"]}')
             data_row_dict = {k: dti[1][INSERT_MAPPING[k]] for k in INSERT_MAPPING.keys()}
+            match = self.check_if_values_changed(data_row_dict)
+
+            if match == 'does not exist':
+                pass
+            else:
+                continue
+
             try:
                 curs.execute(insert_str, data_row_dict)
             except Exception as e:
+                self.conn.rollback()
                 t = e.pgerror
                 if 'duplicate' in t.split(':')[1]:
-                    print('Duplicate encountered, skipping entry')
+                    match = self.check_if_values_changed(data_row_dict)
+                    if match:
+                        print('Duplicate encountered, skipping entry')
+                    if not match:
+                        print('Value changed and has been updated')
                     continue
             self.conn.commit()
 
