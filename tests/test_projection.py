2fb29fdb3fbfae1ac7e37079bc9f73b2ec120aa3
AlexStephensJHUAPL <alexandra.stephens@jhuapl.edu>
2022-08-29 12:46:46 -0400

    Modify projection unit tests and data to use new columns

--- tests/test_projection.py
+++ tests/test_projection.py
@@ -6,7 +6,7 @@ from gap_filling.constants import DatabaseColumns as DbColumns
 
 CO2 = "co2"
 N2O = "n2o"
-ECUADOR = "Ecuador"
+ECUADOR = "ECD"  # No idea if this is the right code, but its a placeholder
 USA = "USA"
 ONE_A = "1.A"
 ONE_B = "1.B"
@@ -22,11 +22,11 @@ def test_regression():
     # Check a few specific values
     assert all(df_project.loc[(df_project[DbColumns.SECTOR] == ONE_B) &
                               (df_project[DbColumns.GAS] == CO2) &
-                              (df_project[DbColumns.COUNTRY] == ECUADOR), DbColumns.VALUE] == np.array([7, 8, 9]))
+                              (df_project[DbColumns.ID] == ECUADOR), DbColumns.VALUE] == np.array([7, 8, 9]))
 
     assert all(df_project.loc[(df_project[DbColumns.SECTOR] == ONE_B) &
                               (df_project[DbColumns.GAS] == N2O) &
-                              (df_project[DbColumns.COUNTRY] == ECUADOR), DbColumns.VALUE] == np.array([14, 16, 18]))
+                              (df_project[DbColumns.ID] == ECUADOR), DbColumns.VALUE] == np.array([14, 16, 18]))
 
 
 def test_baseline_forward_fill():
@@ -40,10 +40,10 @@ def test_baseline_forward_fill():
     # Check a few specific values
     assert all(df_project.loc[(df_project[DbColumns.SECTOR] == ONE_A) &
                               (df_project[DbColumns.GAS] == CO2) &
-                              (df_project[DbColumns.COUNTRY] == ECUADOR), DbColumns.VALUE] == 4)
+                              (df_project[DbColumns.ID] == ECUADOR), DbColumns.VALUE] == 4)
     assert all(df_project.loc[(df_project[DbColumns.SECTOR] == ONE_A) &
                               (df_project[DbColumns.GAS] == N2O) &
-                              (df_project[DbColumns.COUNTRY] == USA), DbColumns.VALUE] == 4.37)
+                              (df_project[DbColumns.ID] == USA), DbColumns.VALUE] == 4.37)
 
 
 def test_data_cleaning():
@@ -61,11 +61,11 @@ def test_data_cleaning():
     # Check that a missing year was filled with nan
     assert np.isnan(proj_edgar.data.loc[(proj_edgar.data[DbColumns.SECTOR] == ONE_A) &
                                         (proj_edgar.data[DbColumns.GAS] == CO2) &
-                                        (proj_edgar.data[DbColumns.COUNTRY] == ECUADOR) &
+                                        (proj_edgar.data[DbColumns.ID] == ECUADOR) &
                                         (proj_edgar.data[DbColumns.YEAR] == 2013), DbColumns.VALUE].item())
     # Check that a certain value remained unchanged
     assert proj_edgar.data.loc[(proj_edgar.data[DbColumns.SECTOR] == ONE_A) &
                                (proj_edgar.data[DbColumns.GAS] == N2O) &
-                               (proj_edgar.data[DbColumns.COUNTRY] == USA) &
+                               (proj_edgar.data[DbColumns.ID] == USA) &
                                (proj_edgar.data[DbColumns.YEAR] == 2018), DbColumns.VALUE].item() == 4.37
     assert proj_edgar.full_df.shape[0] == 18
